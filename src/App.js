4e0f6486dc9e4d7b0fc2963b043d2742ba24ba6d
luke20332 <78817201+luke20332@users.noreply.github.com>
2023-03-09 18:20:15 +0000

    initialisation of the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./TodoList";
+import React, {useEffect, useState } from "react";
+// root of application
+// write the HTML heere
+
+// react manages state, if state chanes, it rerenders
+// redner to dos, so if anythign changes, it rerenders.
+
+// use state hook
+// const []
+// useState returns an array
+// first element is the list of todos 
+// second element is the function which changes the todos
+// object destructuring
+
+// todos = all the todos in the state
+// settodos = what we use to change the,
 
 function App() {
+  const [todos, setTodos] = useState(['eat food', 'ie']) // empty array of todos
+  // todos are props. passed to components, just like attributes to an elemnt
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos}/> 
+      <input type="Text"/>
+      <button> Add Todo</button>
+      <button> clear todos</button>
+      <div>0 left to do</div>
+    </>
+  )
+
+
 }
 
 export default App;
