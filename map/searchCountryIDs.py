19f1b04da9f5a6b4a9c6b557cc611d93bc746c95
olliemyers4 <olliemyers4@gmail.com>
2023-03-14 22:44:29 +0000

    Fixed Bugs
    Now Queries correctly using f strings

--- map/searchCountryIDs.py
+++ map/searchCountryIDs.py
@@ -1,19 +1,20 @@
-from urllib import request
+import requests
 import json
 
 
 
 def searchCountryIDs(id,ids):
-    url = "https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={id}"
-    response = request.urlopen(url)
-    data = json.loads(response.read())
+    url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={id}&lg=1"
+    response = requests.get(url)
+    data = json.loads(response.text)
     if data == json.loads('[{"title":"","from":"1000-01-01","to":"3000-01-01","poly":"","pcr":0,"lmz":0,"ptrn":0,"cnt":[],"opts":{"desc":null,"color":null,"ext":0,"id":0,"type":0,"cz":0,"lineWeight":0},"abbr":"","pabbr":""}]'):
         pass
     else:
         ids.append(id)
-        print(id)
+        print(f"Found ID: {id}")
 
 
 ids = []
-for i in range(1,9999999):
+for i in range(0,9999999):
+    #1050486 - is a valid ID
     searchCountryIDs(i,ids)
