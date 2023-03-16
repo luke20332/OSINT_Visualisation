71a864aefdc6a9858b84e8345d79e7a435a1162e
olliemyers4 <olliemyers4@gmail.com>
2023-03-16 11:24:41 +0000

    New way to pull all ~3400 countries
    Will run and then recommit the file

--- map/searchCountryIDs.py
+++ map/searchCountryIDs.py
@@ -2,24 +2,19 @@ import requests
 import json
 
 
+start = 1048759
+end = 1052137
+current = start
 
-def searchCountryIDs(id,ids):
-    url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={id}&lg=1"
+
+with open('idsJson.txt', 'w') as file:
+    idString= ""
+    while idString.count(",") < 30 & current < end:
+        idString += str(current) + ","
+        current += 1
+    url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={idString}&lg=1"
     response = requests.get(url)
     data = json.loads(response.text)
-    if data == json.loads('[{"title":"","from":"1000-01-01","to":"3000-01-01","poly":"","pcr":0,"lmz":0,"ptrn":0,"cnt":[],"opts":{"desc":null,"color":null,"ext":0,"id":0,"type":0,"cz":0,"lineWeight":0},"abbr":"","pabbr":""}]'):
-        pass
-    else:
-        ids.append(id)
-        #print(f"Found ID: {id}")
+    file.writelines(data)
 
-
-ids = []
-for i in range(1048759,1058759):
-    #1048759 is the earliest I could find thus far
-    #1058759 - is empty
-    searchCountryIDs(i,ids)
-
-print("Done")
-with open('ids.txt', 'w') as file:
-    file.writelines(ids)
+print("done")
