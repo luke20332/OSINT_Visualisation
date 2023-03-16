7dfeacf9e8c7f92aec89d30209b66534db4da67a
olliemyers4 <olliemyers4@gmail.com>
2023-03-16 11:50:38 +0000

    Fixed Loop Bug
    Needed Brackets on conditions

--- map/searchCountryIDs.py
+++ map/searchCountryIDs.py
@@ -1,5 +1,5 @@
 import requests
-
+import time
 
 start = 1048759
 end = 1052137
@@ -9,12 +9,15 @@ current = start
 with open('idsJson.txt', 'w') as file:
     while current < end:
         idString= ""
-        while idString.count(",") < 30 & current < end:
+        print(idString)
+        while (idString.count(",")+1 < 30) & (current < end):
             idString += str(current) + ","
             current += 1
         idString = idString.removesuffix(",")
         url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={idString}&lg=1"
         response = requests.get(url)
         file.writelines(response.text)
+        print(idString)
+        time.sleep(10)
 
 print("done")
