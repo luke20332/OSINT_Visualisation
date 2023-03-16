022fe4cfbfd9dc2ef4e39cba028bbfd8afe0aff3
olliemyers4 <olliemyers4@gmail.com>
2023-03-16 11:30:45 +0000

    Now actually runs through the data
    Rather than just once

--- map/searchCountryIDs.py
+++ map/searchCountryIDs.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 
 start = 1048759
@@ -8,13 +7,14 @@ current = start
 
 
 with open('idsJson.txt', 'w') as file:
-    idString= ""
-    while idString.count(",") < 30 & current < end:
-        idString += str(current) + ","
-        current += 1
-    url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={idString}&lg=1"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    file.writelines(data)
+    while current < end:
+        idString= ""
+        while idString.count(",") < 30 & current < end:
+            idString += str(current) + ","
+            current += 1
+        idString = idString.removesuffix(",")
+        url = f"https://ostellus.com/MapSvc/API/GetBorderOptions?countryIds={idString}&lg=1"
+        response = requests.get(url)
+        file.writelines(response.text)
 
 print("done")
