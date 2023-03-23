bbae14518253a27210fdec8346928859159d7fa5
ashiph-ui <76753545+ashiph-ui@users.noreply.github.com>
2023-03-23 18:08:17 +0000

    Added some plots and removed unnecessary columns of conflicts

--- conflicts/main.py
+++ conflicts/main.py
@@ -1,4 +1,3 @@
-#%%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +6,7 @@ import polars as pl
 from pathlib import Path
 
 # Read the data
-data = pd.read_excel('SIPRI-Milex-data-1949-2022.xlsx', sheet_name='Constant (2021) US$', index_col=0, header=5)
+data = pd.read_excel('OSINT_Visualisation\conflicts\SIPRI-Milex-data-1949-2022.xlsx', sheet_name='Constant (2021) US$', index_col=0, header=5)
 data = data.drop(['Unnamed: 1', 'Notes'], axis=1)
 
 # Unused index names:
@@ -19,10 +18,67 @@ data = data.drop(extra_index, axis=0)
 data = data.drop(data.iloc[0])
 
 data = data.fillna(-1)
-print(data)
-#%%
+data = data.replace(['...', 'xxx'], -1)
+
 # Read the data
-prio_df = pd.read_csv('ucdp-prio-acd-221.csv', index_col=0)
+prio_df = pd.read_csv('OSINT_Visualisation\conflicts\\ucdp-prio-acd-221.csv', index_col=0, header=0)
+
+# Filtering UCDP data
+print(data.head())
 print(prio_df.head())
-# %%
+# remove unnecessary columns
+remove_columns = ['incompatibility', 'territory_name', 'cumulative_intensity', 'type_of_conflict','start_date', 
+        'start_prec', 'start_date2', 'start_prec2', 'ep_end',
+       'ep_end_date', 'ep_end_prec', 'gwno_a', 'gwno_a_2nd', 'gwno_b',
+       'gwno_b_2nd', 'gwno_loc', 'region', 'version']
+
+prio_df = prio_df.drop(remove_columns, axis=1)
+
+# Data Visualization
+# Plotting the data
+# plt.figure(figsize=(20,10))
+# sns.heatmap(data, cmap='coolwarm', linewidths=0.5, linecolor='black', annot=True, fmt='g')
+# plt.title('Military Expenditure by Country')
+# plt.xlabel('Year')
+# plt.ylabel('Country')
+# plt.show()
+
+
+
+# total military expenditure for each country
+total_mil_exp = data.sum(axis=1)
+total_mil_exp = total_mil_exp.sort_values(ascending=False)
+
+# Plotting the data
+plt.figure(figsize=(20,10))
+sns.barplot(x=total_mil_exp.index, y=total_mil_exp.values)
+plt.title('Total Military Expenditure by Country')
+plt.xlabel('Country')
+plt.ylabel('Total Military Expenditure')
+plt.xticks(rotation=90)
+plt.show()
+
+# total military expenditure for each year
+total_mil_exp_year = data.sum(axis=0)
+total_mil_exp_year = total_mil_exp_year.sort_values(ascending=False)
+
+# Plotting the data
+plt.figure(figsize=(20,10))
+sns.barplot(x=total_mil_exp_year.index, y=total_mil_exp_year.values)
+plt.title('Total Military Expenditure by Year')
+plt.xlabel('Year')
+plt.ylabel('Total Military Expenditure')
+plt.xticks(rotation=90)
+plt.show()
+
+
+
+
+
+
+
+
+ 
+
+
 
