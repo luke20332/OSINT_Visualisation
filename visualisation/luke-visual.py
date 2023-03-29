76ae170194f2d8f7cc791fc8f1c9e3dc82ea053c
luke20332 <78817201+luke20332@users.noreply.github.com>
2023-03-29 22:41:50 +0100

    finding the top buyers of weaponry

--- visualisation/luke-visual.py
+++ visualisation/luke-visual.py
@@ -10,8 +10,27 @@ from scipy.stats import multivariate_normal
 
 import imageio.v3 as iio
 
+buyers = {}
+counter = 0
+
+
+
 with open("numerical_data\joined_data_numeric.csv", newline = '') as transferFile:
     fileReader = csv.reader(transferFile, delimiter = ' ', quotechar= '|')
+    headers = next(fileReader)
     for row in fileReader:
-        print(', '.join(row))
+
+        x = ",".join(row) # converts the list into a string
+        x = x.split(',')
         
+        if x[2] not in buyers:  # x[2] is the buyer
+            buyers[x[2]] = int(x[8])
+        else:
+            buyers[x[2]] = buyers[x[2]] + int(x[8])
+        
+        #else:
+        #    break
+
+
+
+# may have to check if country is a double barrel name (united  x, soviet union, north x)
