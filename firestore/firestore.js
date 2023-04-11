30ce7728e250b6de3b7ccd6ba9e817a3fc569f9b
Radmehr Shiadeh <we20153@bristol.ac.uk>
2023-04-11 11:19:53 +0100

    some more setup

--- firestore/firestore.js
+++
@@ -1,7 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
