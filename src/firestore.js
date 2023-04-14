c348036761fd97dc1b240ff5cd84d2c3bfe24ba4
Radmehr Shiadeh <we20153@bristol.ac.uk>
2023-04-14 13:54:45 +0100

    No idea what happened

--- src/firestore.js
+++ src/firestore.js
@@ -1,24 +1,4 @@
-import firebase from "firebase/app"
 import "firebase/database"
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCF1woH6v_CVi9J_5lCfcdQRT9CeDFTmQo",
-  authDomain: "osint-ads-23.firebaseapp.com",
-  databaseURL: "https://osint-ads-23-default-rtdb.firebaseio.com",
-  projectId: "osint-ads-23",
-  storageBucket: "osint-ads-23.appspot.com",
-  messagingSenderId: "813958012003",
-  appId: "1:813958012003:web:f8b2277e401e224e0b6c56",
-  measurementId: "G-6E5TST6JM3"
-};
-
-
-firebase.initializeApp(firebaseConfig)
-const databseref = firebase.database().ref()
-export const datasref = databseref.child("data")
-export default firebase
-
 var admin = require("firebase-admin");
 
 var serviceAccount = require("path/to/serviceAccountKey.json");
