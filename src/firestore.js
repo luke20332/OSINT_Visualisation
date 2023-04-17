8fa78179cd1f246fc6985c49c3d84c4d3e82a5cc
Radmehr Shiadeh <we20153@bristol.ac.uk>
2023-04-17 11:53:15 +0100

    fixed a bunch of shit

--- src/firestore.js
+++ src/firestore.js
@@ -1,4 +1,9 @@
 import "firebase/database"
+import { initializeApp } from 'firebase-admin';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
+
+
+
 var admin = require("firebase-admin");
 
 var serviceAccount = require("path/to/serviceAccountKey.json");
@@ -6,3 +11,19 @@ var serviceAccount = require("path/to/serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF1woH6v_CVi9J_5lCfcdQRT9CeDFTmQo",
+  authDomain: "osint-ads-23.firebaseapp.com",
+  databaseURL: "https://osint-ads-23-default-rtdb.firebaseio.com",
+  projectId: "osint-ads-23",
+  storageBucket: "osint-ads-23.appspot.com",
+  messagingSenderId: "813958012003",
+  appId: "1:813958012003:web:f8b2277e401e224e0b6c56",
+  measurementId: "G-6E5TST6JM3"
+};
+
+const  firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const example = collection(db, 'pass here'); // and example for passing a collection into firestore database
+const docs = await getDocs(example); // get documentations
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import { initializeApp } from 'firebase-admin';
-import ReactDOM from 'react-dom/client';
+import { React } from 'react';
+import { ReactDOM } from 'react-dom/client';
 //import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import { collection, getDocs, getFirestore } from 'firebase/firestore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,21 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCF1woH6v_CVi9J_5lCfcdQRT9CeDFTmQo",
-    authDomain: "osint-ads-23.firebaseapp.com",
-    databaseURL: "https://osint-ads-23-default-rtdb.firebaseio.com",
-    projectId: "osint-ads-23",
-    storageBucket: "osint-ads-23.appspot.com",
-    messagingSenderId: "813958012003",
-    appId: "1:813958012003:web:f8b2277e401e224e0b6c56",
-    measurementId: "G-6E5TST6JM3"
-  };
-
-const  firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-/*const example = collection(db, 'pass here'); // and example for passing a collection into firestore database
-const docs = await getDocs(example); // get documentations*/
-
-
