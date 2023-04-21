4a39dced9c879878bb6255ed40c68912384d78a8
Radmehr Shiadeh <we20153@bristol.ac.uk>
2023-04-21 17:00:55 +0100

    function to draw a line between two circles

--- src/canvas-component/hooks.js
+++ src/canvas-component/hooks.js
@@ -12,16 +12,27 @@ export function useOnDraw() {
     }
 
     function draw([x1, y1], [x2, y2]) {
+        //circle on first point
         if (!ctx) return;
         ctx.beginPath();
         ctx.arc(x1, y1, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "blue";
         ctx.fill();
-    
+        //circle on second point
         ctx.beginPath();
         ctx.arc(x2, y2, 5, 0, 2 * Math.PI);
         ctx.fillStyle = "red";
         ctx.fill();
+
+        //draw line
+        const start = ((x1 + y1)/2);
+        const end = ((x2 + y2)/2);
+        ctx.beginPath();
+        ctx.setLineDash([5, 5]);
+        ctx.moveTo(start[0], start[1]);
+        ctx.lineTo(end[0], end[1]);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
       }
 
     return {setCanvasRef, draw};
