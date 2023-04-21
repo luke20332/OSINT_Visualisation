62e4ad6b534b38ddbf4d5da268fd941a80a71e86
Radmehr Shiadeh <we20153@bristol.ac.uk>
2023-04-21 16:55:49 +0100

    draw method in place not tested yet

--- src/canvas-component/hooks.js
+++ src/canvas-component/hooks.js
@@ -1,12 +1,28 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export function useOnDraw() {
     const canvasRef = useRef(null);
+    const [ctx, setCtx] = useState(null);
 
     function setCanvasRef(ref) {
         if(!ref) return;
         canvasRef.current = ref;
+        const ctx = canvasRef.current.getContext('2d');
+        setCtx(ctx);
     }
 
-    return setCanvasRef;
+    function draw([x1, y1], [x2, y2]) {
+        if (!ctx) return;
+        ctx.beginPath();
+        ctx.arc(x1, y1, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    
+        ctx.beginPath();
+        ctx.arc(x2, y2, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+      }
+
+    return {setCanvasRef, draw};
 };
